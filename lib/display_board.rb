771a962b8dcff12c720528caf95e6df8d06eae01
Arslan Ali <ali.arslan319@gmail.com>
2019-02-09 23:58:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+puts display_board(board)
