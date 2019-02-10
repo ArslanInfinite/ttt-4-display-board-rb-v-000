c39c89de9ad685ae511845f34c8b94a1c8fcc643
Arslan Ali <ali.arslan319@gmail.com>
2019-02-10 02:10:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-rows = ["O", "X", "O", "O", "O", "O", "O", "O", "O"]
+rows = ["O", "X", "O", "O", "X", "O", "O", "O", "O"]
 
 def display_board(rows)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   |#{rows[4}]   |   "
   puts "-----------"
   puts "   |   |   "
 end
